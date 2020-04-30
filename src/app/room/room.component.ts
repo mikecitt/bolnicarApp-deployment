1b2eba04ff298e773b2bfe4c80d69128733f7368
Mikec <mikecitt@gmail.com>
2020-04-30 17:32:02 +0200

    Odradjen merge.

---
+++ src/app/room/room.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from "@angular/forms";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.css']
+})
+export class RoomComponent implements OnInit {
+
+  roomNumber:string = '';
+  roomType:string = null;
+  message:string = null;
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  addRoom() {
+    var formData = {
+      "roomNumber": this.roomNumber,
+      "type"      : this.roomType
+    }
+
+    if(this.roomNumber == "" || this.roomType == null) {
+      this.message = "Sva polja moraju biti popunjena.";
+    }
+    else {
+      return this.http.post<any>('http://localhost:8080/room/add', formData).subscribe(data => {
+        if(data['message'] == "true") {
+          this.message = "Sala uspešno dodata."
+        }
+        else {
+          this.message = "Sala već postoji."
+        }
+      });
+    }
+  }
+}
