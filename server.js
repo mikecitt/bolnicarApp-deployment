8c66e1d747655bb6610976aa1f94774a519ca7af
zdugi <zdravko.dugi@gmail.com>
2020-06-12 02:55:56 +0200

    dodat cors opet

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
-//app.use(cors());
+app.use(cors());
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/client'));
