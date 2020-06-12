78ad1b8537f4ccf22474eb0d418442a354407601
zdugi <zdravko.dugi@gmail.com>
2020-06-12 02:10:39 +0200

    dodat cors u server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/client'));
